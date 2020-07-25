194efcfdd9c09f066ea526334ae083d61862d0a4
Zun <zunsamuel97@gmail.com>
2020-07-25 14:06:40 -0500

    developed basic management api

---
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Anna Smith")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student toAdd){
+        System.out.println(toAdd);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Integer id){
+        System.out.println(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateStudent(@PathVariable Integer id, @RequestBody Student toEdit){
+        System.out.println(String.format("%s %s", id, toEdit));
+    }
+
+}
